c803fbf71f1c7233b3d9ec128e586fd8bc4d3a66
kasbaker <katherine.baker.a@gmail.com>
2020-05-15 16:12:04 -0700

    Pinned setuptools==44.0.0 and in requirements/base.txt added version file to pyinstaller_hooks/hook-ipfx.py

--- pyinstaller_hooks/hook-ipfx.py
+++ pyinstaller_hooks/hook-ipfx.py
@@ -6,4 +6,7 @@ ipfx_dir = os.path.dirname(ipfx.__file__)
 defaults_glob = os.path.join(ipfx_dir, "defaults", "*")
 defaults_dir = os.path.join("ipfx", "defaults")
 
-datas = [(defaults_glob, defaults_dir)]
+version_file = os.path.join(ipfx_dir, "version.txt")
+version_dir = os.path.join("ipfx")
+
+datas = [(defaults_glob, defaults_dir), (version_file, version_dir)]
