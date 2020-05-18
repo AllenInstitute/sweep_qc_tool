10e1559896c0ecbedcc1437d9c29c7ee43955a33
kasbaker <katherine.baker.a@gmail.com>
2020-05-18 12:38:12 -0700

    Fixed bug where blank rows were not removed from sweep view after new data set was loaded.

--- src/main/python/sweep_table_model.py
+++ src/main/python/sweep_table_model.py
@@ -98,10 +98,10 @@ class SweepTableModel(QAbstractTableModel):
 
         # clears any data that the table is currently holding
         if self.rowCount() > 0:
-            # simply resetting the model here is easier than calling dataChanged()
+            # Simply resetting the model here is easier than removing the rows
+            # and then informing SweepTableView that the data has changed.
             self.beginResetModel()
             self._data = []
-            # self.removeRows(0, self.rowCount(), QModelIndex())
             self.endResetModel()
 
         plotter = SweepPlotter(data_set, self.plot_config)
@@ -138,17 +138,6 @@ class SweepTableModel(QAbstractTableModel):
 
         # emit signal indicating that the model now has new data in it
         self.new_data.emit()
-
-    def removeRows(self, start_row: int, end_row: int, parent=None, *args, **kwargs):
-        self.beginRemoveRows(parent, start_row, end_row)
-        for row in reversed(range(start_row, end_row)):
-            self.removeRow(row, parent)
-        self.endRemoveRows()
-        return True
-
-    def removeRow(self, row_index: int, parent=None, *args, **kwargs):
-        self._data.pop(row_index)
-        return True
 
     def rowCount(self, *args, **kwargs):
         """ The number of rows in the sweep table model, which should be the
