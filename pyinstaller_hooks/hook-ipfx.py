cc1b6ae1dc628eb04c16c456181cfedd79e02767
nilegraddis <nilegraddis@gmail.com>
2020-05-28 16:02:16 -0700

    is version.txt the issue?

--- pyinstaller_hooks/hook-ipfx.py
+++ pyinstaller_hooks/hook-ipfx.py
@@ -6,4 +6,8 @@ ipfx_dir = os.path.dirname(ipfx.__file__)
 defaults_glob = os.path.join(ipfx_dir, "defaults", "*")
 defaults_dir = os.path.join("ipfx", "defaults")
 
-datas = [(defaults_glob, defaults_dir)]
+# borrowing this temporarily from @kasbaker's c803fbf71f1c7233b3d9ec128e586fd8bc4d3a66
+version_file = os.path.join(ipfx_dir, "version.txt")
+version_dir = os.path.join("ipfx")
+
+datas = [(defaults_glob, defaults_dir), (version_file, version_dir)]
