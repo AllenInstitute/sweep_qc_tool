51d163cd3f5562eb6d4cef4a33f9258692983c15
nilegraddis <nilegraddis@gmail.com>
2020-05-28 16:49:22 -0700

    cant re-use spec dir for the glob - second item is within-bundle path which must be relative

--- pyinstaller_hooks/hook-hdmf.py
+++ pyinstaller_hooks/hook-hdmf.py
@@ -3,7 +3,7 @@ import os
 import hdmf
 
 hdmf_dir = os.path.dirname(hdmf.__file__)
-spec_dir = os.path.join(hdmf_dir, "common", "hdmf-common-schema", "common")
-spec_glob = os.path.join(spec_dir, "*.yaml")
+spec_dir = os.path.join("hdmf", "common", "hdmf-common-schema", "common")
+spec_glob = os.path.join(hdmf_dir, "common", "hdmf-common-schema", "common", "*.yaml")
 
 datas = [(spec_glob, spec_dir)]
