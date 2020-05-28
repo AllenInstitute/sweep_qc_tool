8d4d529859b7553b8e3ba213c0a8de569b203ce6
nilegraddis <nilegraddis@gmail.com>
2020-05-28 16:32:15 -0700

    should probably import hdmf into the hdmf hook

--- pyinstaller_hooks/hook-hdmf.py
+++ pyinstaller_hooks/hook-hdmf.py
@@ -1,9 +1,9 @@
 import os
 
-import pynwb
+import hdmf
 
 hdmf_dir = os.path.dirname(hdmf.__file__)
-spec_dir = os.path.join(hdmf_dir, "common", "hdmf-common-schema")
+spec_dir = os.path.join(hdmf_dir, "common", "hdmf-common-schema", "common")
 spec_glob = os.path.join(spec_dir, "*.yaml")
 
 datas = [(spec_glob, spec_dir)]
